e0472a1ea5d37c36c4fdf93275f8a84ae6d7e8c0
JumpLink <jumplink@gmail.com>
2013-11-11 17:11:07 +0100

    fix example

--- example/product.js
+++ example/product.js
@@ -1,13 +1,15 @@
 // sublime: tab_size 2; translate_tabs_to_spaces true
 
-var config = require('json-fs').open(__dirname+"/../../../config/general.json");
+var config = require(__dirname+"/../config.json");
 
-var magento = require('../lib/magento.js')(config.mageplus);
+var magento = require('../lib/magento.js')(config.magento);
 
-// magento.auto.catalog.product.info("021-198-009/B", config.mageplus.store_view[0].code, function (error, result) {
+// magento.xmlrpc.auto.catalog.product.info("021-198-009/B", config.mageplus.store_view[0].code, function (error, result) {
 //   console.log(result);
 // });
-var filter = magento.auto.set_filter.sku("021-198-009/B");
-magento.auto.catalog.product.list(filter, config.mageplus.store_view[0].code, function (error, result) {
+var filter = magento.xmlrpc.auto.set_filter.sku("021-198-009/B");
+console.log(filter);
+magento.xmlrpc.auto.catalog.product.list({ sku: { like: '%' } }, "shop_de", function (error, result) {
+  console.log(error);
   console.log(result);
 });
