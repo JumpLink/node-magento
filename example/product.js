b4e11972aa836fd6784edf5c419cf70aab4f05cd
JumpLink <pascal@jumplink.eu>
2015-05-15 17:47:18 +0200

    test if example is working

--- example/product.js
+++ example/product.js
@@ -1,15 +1,32 @@
 // sublime: tab_size 2; translate_tabs_to_spaces true
 
-var config = require(__dirname+"/../config.json");
+var _config = require("./config.json");
 
-var magento = require('../lib/magento.js')(config.magento);
+var config = {
+  "host": _config.host,
+  "port": _config.port || 80,
+  "xmlrpc_path": _config.xmlrpc_path || "/index.php/api/xmlrpc/",
+  "login": _config.login,
+  "pass": _config.pass
+} 
 
-// magento.xmlrpc.auto.catalog.product.info("021-198-009/B", config.mageplus.store_view[0].code, function (error, result) {
+var magento = require('../lib/magento.js')(config);
+
+// console.log(magento);
+
+// magento.xmlrpc.manual.init(function(err) {
+//   console.log(err);
+//   console.log(magento);
+// });
+
+// magento.xmlrpc.auto.catalog.product.info("021-198-009/B", "shop_de", function (error, result) {
 //   console.log(result);
 // });
+
 var filter = magento.xmlrpc.auto.set_filter.sku("021-198-009/B");
+
 console.log(filter);
-magento.xmlrpc.auto.catalog.product.list({ sku: { like: '%' } }, "shop_de", function (error, result) {
+magento.xmlrpc.auto.catalog.product.list(filter, "shop_de", function (error, result) {
   console.log(error);
   console.log(result);
 });
